efe09f59543b4c6130b126b24b3dcc9952f25a0b
Jason Schroeder <jasonsch@gmail.com>
2022-01-01 21:52:57 -0800

    Add skeleton 'monitors' command

---
+++ monitors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace monitors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                Console.WriteLine($"screen ==> {screen.WorkingArea} / {screen.Bounds} / {screen.DeviceName} / {screen.Primary}");
+            }
+        }
+    }
+}
